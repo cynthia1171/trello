cc265b89c5d39791538af92579b1ad11a41fe236
cynthia1171 <c.isla1171@gmail.com>
2017-11-07 22:47:48 -0300

    Versión 0.0.1

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var lista = document.getElementById('lista');
+
+lista.addEventListener('click', function() {
+    var box = document.getElementsByClassName('cont')[0];
+    if (box.style.display = 'none') {
+        box.style.display = 'block';
+    }
+})
